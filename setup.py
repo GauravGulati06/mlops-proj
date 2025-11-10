3a9e96c4a45e9dbc5f0347b4a5852278d9eee072
GauravGulati06 <gauravgulati619@gmail.com>
2025-11-10 20:56:02 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurav Gulati",
+    author_email="gauravgulati619@gmail.com",
+    packages=find_packages()
+)
